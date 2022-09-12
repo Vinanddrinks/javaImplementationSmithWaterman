0f2941bdd8a29152e2e9a5a80ec63d02fb7d8b72
Vincent Labouret <66210430+Vinanddrinks@users.noreply.github.com>
2022-09-12 00:59:12 -0400

    Finished Implementation of MatrixList.java
    - will need comment and review
    
    fya : added .idea folder relative to jetbrain dev env

--- src/MatrixList.java
+++ src/MatrixList.java
@@ -2,34 +2,68 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class MatrixList<T> implements Cloneable,Serializable {
-    private ArrayList<ArrayList<T>> matrix;
-    private int sizeVert,sizeHorz;
+    private final ArrayList<ArrayList<T>> matrix;
+    private int verticalSize, horizontalSize;
 
-    MatrixList(int column){
-        sizeHorz = 0;
-        sizeVert = 1;
-        if(column <= 0){
-            throw new IllegalArgumentException("matrix need at least one column");
+    MatrixList(int column,int row,T standard){
+        horizontalSize = 1;
+        verticalSize = 0;
+        if(column <= 0 || row <= 0){
+            throw new IllegalArgumentException("matrix need at least one column and one row");
         }
-        matrix = new ArrayList<ArrayList<T>>();
-        for (int i = 0; i < column -1; i++) {
-            matrix.add(new ArrayList<T>());
-            sizeVert++;
+        matrix = new ArrayList<>();
+        for (int i = 0; i < column; i++) {
+            addColumn(standard);
+        }
+        for (int i = 0; i < row-1; i++) {
+            addRow(standard);
         }
     }
-    public ArrayList<T> getColumn(int column){
-        if(0 > column && column < sizeVert)throw new IndexOutOfBoundsException("not inside the matrix");
+    private ArrayList<T> getColumn(int column){
+        if(0 > column && column < verticalSize)throw new IndexOutOfBoundsException("not inside the matrix");
         return matrix.get(column);
     }
     public T getValue(int column,int row){
-        if(0 > column && column < sizeVert && 0 > row && row < getColumn(column).size())throw new IndexOutOfBoundsException("not inside the matrix");
+        if(isOffMatrix(column,row))throw new IndexOutOfBoundsException("not inside the matrix");
         return getColumn(column).get(row);
 
     }
 
-    public static void main(String[] args) {
-        MatrixList<String> hello = new MatrixList<String>(0);
-        System.out.println(hello.sizeVert);
-        System.out.println(hello.getValue(3, 0));
+    public void addRow(T value){// add a row with given value
+        for (int i = 0; i < verticalSize; i++) {
+            getColumn(i).add(value);
+        }
+        horizontalSize++;
+    }
+    public void addColumn(T value){// add a row with given value
+        matrix.add(new ArrayList<>());
+        for (int i = 0; i < horizontalSize; i++) {
+            getColumn(verticalSize).add(value);
+        }
+        verticalSize++;
+    }
+
+    public void setValue(int vertical,int horizontal,T value){
+        if(isOffMatrix(vertical,horizontal))throw new IndexOutOfBoundsException("not inside the matrix");
+        matrix.get(vertical).set(horizontal,value);
+    }
+    private boolean isOffMatrix(int vertical, int horizontal){
+        return !( (vertical >= 0 && vertical < verticalSize) && (horizontal >= 0 && horizontal < horizontalSize));
+    }
+
+    public static void main(String[] args) { //class test function
+        MatrixList<String> hello = new MatrixList<>(4, 5, "Hello");
+        System.out.println(hello.verticalSize+"*"+hello.horizontalSize);
+        System.out.println(hello.getValue(4, 4));
+    }
+    @Override
+    public MatrixList<T> clone(){
+        MatrixList<T> clone = new MatrixList<>(verticalSize, horizontalSize, getValue(0, 0));
+        for (int i = 0; i < verticalSize; i++) {
+            for (int j = 0; j < horizontalSize; j++) {
+                clone.setValue(i,j,getValue(i,j));
+            }
+        }
+        return clone;
     }
 }
